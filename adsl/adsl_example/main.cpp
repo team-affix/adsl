dbf2b524d96263e16118fc9b19184c2d5dc20ee8
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2022-09-07 11:49:38 -0700

    Updated affix-base and affix-services

--- adsl/adsl_example/main.cpp
+++ adsl/adsl_example/main.cpp
@@ -7,13 +7,15 @@
 
 using namespace aurora;
 
-
-
 int main(
 
 )
 {
+	asio::io_context l_io_context;
 
+	affix_base::data::ptr<affix_services::client_configuration> l_client_configuration = new affix_services::client_configuration("");
+
+	affix_services::client l_client(l_io_context, new affix_services::client_configuration(""));
 
 	return 0;
 
